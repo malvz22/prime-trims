ac32012c1865e149c1392354ad70268719709979
Malvin A <malvinaudriansyah77@gmail.com>
2025-08-11 15:18:25 +0700

    feat: update color scheme to white and add Linktree ahchor link on CTA button

--- src/App.jsx
+++ src/App.jsx
@@ -1,92 +1,153 @@
-import { Brush, Droplets, Scissors, Thermometer } from 'lucide-react'
-import './App.css'
-import Header from './components/Header/Header.jsx'
-import Footer from './components/Footer/Footer.jsx'
-import { TbRazorElectric } from 'react-icons/tb'
+import { Brush, Droplets, Scissors, Thermometer } from "lucide-react";
+import "./App.css";
+import Header from "./components/Header/Header.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+import { TbRazorElectric } from "react-icons/tb";
 
 function App() {
-
   return (
     <>
-      <Header/>
-      <main className='w-full max-w-[1024px] px-4 mx-auto'>
-        <section id='home' className="flex flex-col items-center justify-center min-h-screen text-center">
-          <img src='/primetrims-logo.png' className='' />
+      <Header />
+      <main className="w-full max-w-[1024px] px-4 mx-auto">
+        <section
+          id="home"
+          className="flex flex-col items-center justify-center min-h-screen text-center"
+        >
+          <img src="/primetrims-logo.png" className="" />
           <h1>Haircuts for Everyone!</h1>
-          <h2 className='text-white mb-4'>Transform your look with us!</h2>
-          <button>Book Now!</button>
+          <h2 className="mb-4">Transform your look with us!</h2>
+          <a
+            href="https://linktr.ee/primetrims?utm_source=linktree_profile_share&ltsid=7f35900f-0d17-413a-b3c3-44d1ec13c5f1"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button>Book Now!</button>
+          </a>
         </section>
-        <section id='about-us' className='text-center flex flex-col justify-center scroll-mt-22 py-4 min-h-screen'>
+        <section
+          id="about-us"
+          className="text-center flex flex-col justify-center scroll-mt-22 py-4 min-h-screen"
+        >
           <h1>Welcome to PRIME TRIMS</h1>
-          <div className='flex flex-row w-full max-w-full gap-3'>
-            <img src='/barber.jpg' className='w-[50%] object-cover rounded-2xl' />
-            <p className='text-start'>Step into your new story. Prime Trims is more than just a barbershop — it's a space to relax, recharge, and transform your style. Located in the heart of the city, we bring quality grooming and a personal touch to every visit.</p>
+          <div className="flex flex-row w-full max-w-full gap-3">
+            <img
+              src="/barber.jpg"
+              className="w-[50%] object-cover rounded-2xl"
+            />
+            <p className="text-start">
+              Step into your new story. Prime Trims is more than just a
+              barbershop — it's a space to relax, recharge, and transform your
+              style. Located in the heart of the city, we bring quality grooming
+              and a personal touch to every visit.
+            </p>
           </div>
         </section>
-        <section id='services' className='text-center flex flex-col justify-center scroll-mt-22'>
+        <section
+          id="services"
+          className="text-center flex flex-col justify-center scroll-mt-22"
+        >
           <h1>Services</h1>
-          <p className='text-white mb-4'>At Prime Trims, we believe in providing top-notch grooming services that cater to your unique style and preferences. Our skilled barbers are dedicated to delivering exceptional haircuts, shaves, and grooming experiences that leave you looking sharp and feeling confident.</p>
-          <div className='grid grid-cols-1 md:grid-cols-2 gap-12 mb-12'>
-            <div className='w-full max-w-full aspect-[2/1] justify-center items-center flex flex-col gap-3'>
+          <p className="text-white mb-4">
+            At Prime Trims, we believe in providing top-notch grooming services
+            that cater to your unique style and preferences. Our skilled barbers
+            are dedicated to delivering exceptional haircuts, shaves, and
+            grooming experiences that leave you looking sharp and feeling
+            confident.
+          </p>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-12 mb-12">
+            <div className="w-full max-w-full aspect-[2/1] justify-center items-center flex flex-col gap-3">
               <Scissors size={48} />
               <h3>Haircut</h3>
-              <p>Precision haircuts tailored to your style. Whether it's a classic taper, modern fade, or trendy crop, our barbers ensure you walk out sharp and confident.</p>
+              <p>
+                Precision haircuts tailored to your style. Whether it's a
+                classic taper, modern fade, or trendy crop, our barbers ensure
+                you walk out sharp and confident.
+              </p>
             </div>
-            <div className='w-full max-w-full aspect-[2/1] justify-center items-center flex flex-col gap-3'>
+            <div className="w-full max-w-full aspect-[2/1] justify-center items-center flex flex-col gap-3">
               <Droplets size={48} />
               <h3>Hairwash</h3>
-              <p>A relaxing hair wash using premium shampoo and scalp massage techniques to cleanse and refresh your hair before or after your haircut.</p>
+              <p>
+                A relaxing hair wash using premium shampoo and scalp massage
+                techniques to cleanse and refresh your hair before or after your
+                haircut.
+              </p>
             </div>
-            <div className='w-full max-w-full aspect-[2/1] justify-center items-center flex flex-col gap-3'>
+            <div className="w-full max-w-full aspect-[2/1] justify-center items-center flex flex-col gap-3">
               <Thermometer size={48} />
               <h3>Hot Towel</h3>
-              <p>Experience a soothing hot towel treatment that opens pores, softens facial hair, and preps your skin for the ultimate shave or relaxation.</p>
+              <p>
+                Experience a soothing hot towel treatment that opens pores,
+                softens facial hair, and preps your skin for the ultimate shave
+                or relaxation.
+              </p>
             </div>
-            <div className='w-full max-w-full aspect-[2/1] justify-center items-center flex flex-col gap-3'>
+            <div className="w-full max-w-full aspect-[2/1] justify-center items-center flex flex-col gap-3">
               <TbRazorElectric size={48} />
               <h3>Shaving</h3>
-              <p>A clean, smooth shave using traditional straight razors and warm lather for precision and comfort. Includes post-shave balm for refreshed skin.</p>
+              <p>
+                A clean, smooth shave using traditional straight razors and warm
+                lather for precision and comfort. Includes post-shave balm for
+                refreshed skin.
+              </p>
             </div>
-            <div className='w-full max-w-full md:max-w-[476px] mx-auto aspect-[2/1] justify-center items-center flex flex-col gap-3 col-span-1 md:col-span-2'>
+            <div className="w-full max-w-full md:max-w-[476px] mx-auto aspect-[2/1] justify-center items-center flex flex-col gap-3 col-span-1 md:col-span-2">
               <Brush size={48} />
               <h3>Basic Coloring</h3>
-              <p>Enhance or refresh your look with basic hair coloring services. Ideal for covering grays or trying subtle tones without full dye jobs.</p>
+              <p>
+                Enhance or refresh your look with basic hair coloring services.
+                Ideal for covering grays or trying subtle tones without full dye
+                jobs.
+              </p>
             </div>
           </div>
         </section>
-        <section id='pricelist' className='text-center flex flex-col justify-center scroll-mt-22 min-h-screen'>
+        <section
+          id="pricelist"
+          className="text-center flex flex-col justify-center scroll-mt-22 min-h-screen"
+        >
           <h1>Price List</h1>
-          <div className='flex flex-col mb-12'>
-            <div className='flex flex-row justify-between'>
-              <h2 className='font-semibold'>Haircut + Wash + Hot Towel</h2>
-              <h2 className='font-bold'>70K</h2>
+          <div className="flex flex-col mb-12">
+            <div className="flex flex-row justify-between">
+              <h2 className="font-semibold">Haircut + Wash + Hot Towel</h2>
+              <h2 className="font-bold">70K</h2>
             </div>
-            <hr className='border-t border-gray-300 my-3' />
-            <div className='flex flex-row justify-between'>
-              <h2 className='font-semibold'>Shaving</h2>
-              <h2 className='font-bold'>50K</h2>
+            <hr className="border-t border-gray-300 my-3" />
+            <div className="flex flex-row justify-between">
+              <h2 className="font-semibold">Shaving</h2>
+              <h2 className="font-bold">50K</h2>
             </div>
-            <hr className='border-t border-gray-300 my-3' />
-            <div className='flex flex-row justify-between'>
-              <h2 className='font-semibold'>Basic Coloring</h2>
-              <h2 className='font-bold'>100K</h2>
+            <hr className="border-t border-gray-300 my-3" />
+            <div className="flex flex-row justify-between">
+              <h2 className="font-semibold">Basic Coloring</h2>
+              <h2 className="font-bold">100K</h2>
             </div>
           </div>
-          <button className='w-full max-w-[200px] mx-auto'>Book Now!</button>
+          <a
+            href="https://linktr.ee/primetrims?utm_source=linktree_profile_share&ltsid=7f35900f-0d17-413a-b3c3-44d1ec13c5f1"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button>Book Now!</button>
+          </a>
+          {/* <button className="w-full max-w-[200px] mx-auto">Book Now!</button> */}
         </section>
-        <section id='gallery' className='text-center flex flex-col justify-center scroll-mt-22'>
+        <section
+          id="gallery"
+          className="text-center flex flex-col justify-center scroll-mt-22"
+        >
           <h1>Gallery</h1>
-          <div className='grid grid-cols-1 md:grid-cols-2 gap-4 mb-12'>
-            <img src='/result1.webp' />
-            <img src='/result2.webp' />
-            <img src='/result3.webp' />
-            <img src='/result4.webp' />
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-12">
+            <img src="/result1.webp" />
+            <img src="/result2.webp" />
+            <img src="/result3.webp" />
+            <img src="/result4.webp" />
           </div>
         </section>
       </main>
-      <Footer/>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <header className="w-full max-w-full flex items-center justify-between p-4 bg-deepIndigo text-white sticky top-0 z-50">
+    <header className="w-full max-w-full flex items-center justify-between p-4 bg-white text-[#1f2937] shadow-md sticky top-0 z-50">
       <div className="flex flex-row justify-between mx-auto w-full max-w-[1024px] items-center">
         <a href="/">
           <img src="/primetrims-logo.png" className="w-[80px]" />
@@ -9,31 +9,31 @@ const Header = () => {
         <div className="flex flex-row gap-4">
           <a
             href="#home"
-            className="text-white hover:text-mint transition-colors duration-300 font-semibold text-lg"
+            className="hover:text-mint transition-colors duration-300 font-semibold text-lg"
           >
             Home
           </a>
           <a
             href="#about-us"
-            className="text-white hover:text-mint transition-colors duration-300 font-semibold text-lg"
+            className="hover:text-mint transition-colors duration-300 font-semibold text-lg"
           >
             About Us
           </a>
           <a
             href="#services"
-            className="text-white hover:text-mint transition-colors duration-300 font-semibold text-lg"
+            className="hover:text-mint transition-colors duration-300 font-semibold text-lg"
           >
             Services
           </a>
           <a
             href="#pricelist"
-            className="text-white hover:text-mint transition-colors duration-300 font-semibold text-lg"
+            className="hover:text-mint transition-colors duration-300 font-semibold text-lg"
           >
             Pricelist
           </a>
           <a
             href="#gallery"
-            className="text-white hover:text-mint transition-colors duration-300 font-semibold text-lg"
+            className="hover:text-mint transition-colors duration-300 font-semibold text-lg"
           >
             Gallery
           </a>
